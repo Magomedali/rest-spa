c7bdb7d99541dae3f284554efc5e1f94a90db3bb
Magomedali <web-ali@yandex.ru>
2019-10-04 19:04:33 +0300

    Integrated php-message implementation library - zend diactoros

--- api/public/index.php
+++ api/public/index.php
@@ -1,3 +1,14 @@
 <?php
-phpinfo();
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\JsonResponse;
+
+chdir(dirname(__DIR__));
+require "vendor/autoload.php";
+
+$request = ServerRequestFactory::fromGlobals();
+
+
+$response = (new JsonResponse(['success'=>1]));
+
+echo $response->getBody()->getContents();
 ?>
