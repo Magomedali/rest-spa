d1c88fa58312a57c9f9a21bb5edbc724fd2edb21
Magomedali <web-ali@yandex.ru>
2019-10-05 15:40:02 +0300

    Added doctrine configs and console commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -10,6 +10,13 @@ require 'vendor/autoload.php';
  * @var \Psr\Container\ContainerInterface $container
  */
 $container = require 'config/container.php';
+
 $cli = new Application('Application console');
 
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
+
+$commands = $container->get('config')['console']['commands'];
+foreach ($commands as $command) {
+	$cli->add($container->get($command));
+}
 $cli->run();
---
+++ api/config/autoload/console.global.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Console\Command;
+
+return [
+	'dependencies'=>[
+        'factories' => [
+            Command\CacheClearCommand::class => Infrastructure\App\Console\Command\CacheClearCommandFactory::class,
+        	Doctrine\Migrations\Tools\Console\Command\DiffCommand::class => Infrastructure\App\Doctrine\Factory\DiffCommandFactory::class,
+        ],
+	],
+	'console' => [
+        'commands' => [
+            Command\CacheClearCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\ExecuteCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\GenerateCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\LatestCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\MigrateCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\DiffCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\UpToDateCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\StatusCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\VersionCommand::class,
+        ],
+        'cachePaths' => [
+            'doctrine' => 'var/cache/doctrine',
+        ],
+    ],
+];
