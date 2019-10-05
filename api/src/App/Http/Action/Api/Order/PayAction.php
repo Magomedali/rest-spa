b0ab9bd1c883fd56a63f8b68d3462a87048d5ceb
Magomedali <web-ali@yandex.ru>
2019-10-06 01:42:11 +0300

    Update PayAction

--- api/src/App/Http/Action/Api/Order/PayAction.php
+++ api/src/App/Http/Action/Api/Order/PayAction.php
@@ -6,15 +6,35 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
+use App\Module\Sale\UseCase\Order\Pay;
+
 class PayAction implements RequestHandlerInterface
 {
 
+	private $handler;
 
+	
+	public function __construct(Pay\Handler $handler)
+	{
+		$this->handler = $handler;
+	}
 	
 	public function handle(ServerRequestInterface $request): ResponseInterface
 	{
 
-		return new JsonResponse(['handle'=>'Buy product'],200);
+		$body = $request->getParsedBody();
+
+		try {
+			$command = new Pay\Command($body['id'],$body['sum']);
+
+			$this->handler->handle($command);
+
+			return new JsonResponse(['success'=>true],200);
+			
+		} catch (\Exception $e) {
+
+			return new JsonResponse(['error'=>$e->getMessage()],500);
+		}
 	}
 
 	public function __invoke(ServerRequestInterface $request): ResponseInterface
