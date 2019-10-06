7dd8c893f33c216c9d026651e6d1f87e4611012b
Magomedali <web-ali@yandex.ru>
2019-10-06 21:15:24 +0300

    Added Validation Middleware and Exception

--- api/config/pipeline.php
+++ api/config/pipeline.php
@@ -5,5 +5,6 @@ use App\Http\Middleware;
 /** @var \Framework\Http\Application $app */
 $app->pipe(Middleware\CredentialsMiddleware::class);
 $app->pipe(Framework\Http\Middleware\BodyParamsMiddleware::class);
+$app->pipe(Framework\Http\Middleware\ValidationExceptionMiddleware::class);
 $app->pipe(Framework\Http\Middleware\RouteMiddleware::class);
 $app->pipe(Framework\Http\Middleware\DispatchMiddleware::class);
---
+++ api/src/App/Http/ValidationException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Api\Http;
+
+
+class ValidationException extends \LogicException
+{
+
+}
