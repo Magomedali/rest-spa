3be766c216680e8c4607e03089361ffec6a5f40f
Magomedali <web-ali@yandex.ru>
2019-10-06 17:31:50 +0300

    Change api route

--- api/config/routes.php
+++ api/config/routes.php
@@ -2,7 +2,7 @@
 use App\Http\Action\Api;
 
 /** @var \Framework\Http\Application $app */
-$app->get('list','/',Api\Product\ListAction::class);
+$app->get('list','/product',Api\Product\ListAction::class);
 $app->put('generate','/product',Api\Product\GenerateAction::class);
 $app->put('order','/order',Api\Order\CreateAction::class);
 $app->post('pay','/pay',Api\Order\PayAction::class);
