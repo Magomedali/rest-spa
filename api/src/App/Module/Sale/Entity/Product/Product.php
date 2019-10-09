0339e7a766620ab2eb856399baa825b3cc34ed8b
Magomedali <web-ali@yandex.ru>
2019-10-09 15:05:19 +0300

    Added strict types

--- api/src/App/Module/Sale/Entity/Product/Product.php
+++ api/src/App/Module/Sale/Entity/Product/Product.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace App\Module\Sale\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
--- api/src/App/Module/Sale/UseCase/Order/Pay/Handler.php
+++ api/src/App/Module/Sale/UseCase/Order/Pay/Handler.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace App\Module\Sale\UseCase\Order\Pay;
 
 use App\Module\Sale\Entity\Order;
